6d270804bccd4773c1a0c5cbd5ddac6bdf39354b
Younes Jaaidi <younes@marmicode.io>
2025-05-05 16:43:14 +0200

    test: ✅ replace spying stub with fake

--- apps/whiskmate/src/app/recipe/recipe-search.ng.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.ng.spec.ts
@@ -1,30 +1,27 @@
 import { render } from '@testing-library/angular';
 import { screen } from '@testing-library/dom';
 import { userEvent } from '@testing-library/user-event';
-import { describe, expect, it, Mocked, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import RecipeSearch from './recipe-search.ng';
-import { RecipeRepository, RecipeRepositoryDef } from './recipe-repository';
 import { of } from 'rxjs';
 import { recipeMother } from '../testing/recipe.mother';
+import {
+  provideRecipeRepositoryFake,
+  RecipeRepositoryFake,
+} from './recipe-repository.fake';
 
 describe(RecipeSearch.name, () => {
   it('should search recipes without filtering', async () => {
-    const { findRecipeTitles, repo } = await renderComponent();
+    const { findRecipeTitles } = await renderComponent();
 
     const els = await findRecipeTitles();
     expect.soft(els).toHaveLength(2);
     expect.soft(els[0]).toHaveTextContent('Burger');
     expect.soft(els[1]).toHaveTextContent('Salad');
-
-    expect(repo.search).toHaveBeenCalledExactlyOnceWith(undefined);
   });
 
   it('should filter recipes using keywords', async () => {
-    const { findRecipeTitles, typeKeywords, burger, repo } =
-      await renderComponent();
-
-    repo.search.mockClear();
-    repo.search.mockReturnValue(of([burger]));
+    const { findRecipeTitles, typeKeywords } = await renderComponent();
 
     await typeKeywords('Bur');
 
@@ -33,27 +30,22 @@ describe(RecipeSearch.name, () => {
       expect(els).toHaveLength(1);
       expect(els[0]).toHaveTextContent('Burger');
     });
-
-    expect(repo.search).toHaveBeenCalledExactlyOnceWith('Bur');
   });
 
   async function renderComponent() {
     const burger = recipeMother.withBasicInfo('Burger').build();
     const salad = recipeMother.withBasicInfo('Salad').build();
 
-    const repo: Mocked<RecipeRepositoryDef> = {
-      search: vi.fn(),
-    };
-
-    repo.search.mockReturnValue(of([burger, salad]));
-
     await render(RecipeSearch, {
-      providers: [{ provide: RecipeRepository, useValue: repo }],
+      providers: [provideRecipeRepositoryFake()],
+      configureTestBed(testBed) {
+        testBed.inject(RecipeRepositoryFake).configure({
+          recipes: [burger, salad],
+        });
+      },
     });
 
     return {
-      burger,
-      repo,
       async findRecipeTitles() {
         return screen.findAllByRole('heading', { level: 2 });
       },
