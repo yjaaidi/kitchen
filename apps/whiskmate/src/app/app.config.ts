f2c3829fe0069d7b4fd9c8a0a49c8cd1f62e9eab
Younes Jaaidi <younes@marmicode.io>
2025-05-05 17:18:03 +0200

    feat: ✨ set up app to use fake repo

--- apps/whiskmate/src/app/app.config.ts
+++ apps/whiskmate/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+} from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -6,11 +10,25 @@ import {
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  provideRecipeRepositoryFake,
+  RecipeRepositoryFake,
+} from './recipe/recipe-repository.fake';
+import { recipeMother } from './testing/recipe.mother';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideHttpClient(),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideRecipeRepositoryFake(),
+    provideAppInitializer(() => {
+      const burger = recipeMother.withBasicInfo('Burger').build();
+      const salad = recipeMother.withBasicInfo('Salad').build();
+      const beer = recipeMother.withBasicInfo('Beer').build();
+      inject(RecipeRepositoryFake).configure({
+        recipes: [burger, salad, beer],
+      });
+    }),
   ],
 };
