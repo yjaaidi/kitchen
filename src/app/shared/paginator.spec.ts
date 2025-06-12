848d9513c5c1e4fbd9d3353f1bac54092122e9b1
Younes Jaaidi <younes@marmicode.io>
2025-06-12 17:49:52 +0200

    test: add todo tests

--- src/app/shared/paginator.spec.ts
+++ src/app/shared/paginator.spec.ts
@@ -1,25 +1,45 @@
 import { describe, it } from 'vitest';
+import { screen } from '@testing-library/angular';
+import { RecipePaginator } from './paginator.ng';
+import { t } from '../testing/ng-test-utils';
+import userEvent from '@testing-library/user-event';
 
 describe('RecipePaginator', () => {
-  it.todo('Disables previous button on first page', () => {
-    // Mount the paginator with offset 0 and limit 5, and total 7
-    // Assert the previous button is disabled
+  it.todo('Disables previous button on first page', async () => {
+    await t.mount(RecipePaginator, {
+      inputs: { offset: 0, limit: 5, total: 7 },
+    });
+    const prevButton = screen.getByRole('button', { name: /previous/i });
+    expect(prevButton).toBeDisabled();
   });
 
-  it.todo('Disables next button on last page', () => {
-    // Mount the paginator with offset 5, limit 5, and total 7
-    // Assert the next button is disabled
+  it.todo('Disables next button on last page', async () => {
+    await t.mount(RecipePaginator, {
+      inputs: { offset: 5, limit: 5, total: 7 },
+    });
+    const nextButton = screen.getByRole('button', { name: /next/i });
+    expect(nextButton).toBeDisabled();
   });
 
-  it.todo('Emits offsetChange when next is clicked', () => {
-    // Mount the paginator with offset 0, limit 5, and total 7
-    // Click the next button
-    // Assert offsetChange is emitted with value 5
+  it.todo('Emits offsetChange when next is clicked', async () => {
+    const offsetChange = vi.fn<(offset: number) => void>();
+    await t.mount(RecipePaginator, {
+      inputs: { offset: 0, limit: 5, total: 7 },
+      outputs: { offsetChange },
+    });
+    const nextButton = screen.getByRole('button', { name: /next/i });
+    await userEvent.click(nextButton);
+    expect(offsetChange).toHaveBeenCalledExactlyOnceWith(5);
   });
 
-  it.todo('Emits offsetChange when previous is clicked', () => {
-    // Mount the paginator with offset 5, limit 5, and total 7
-    // Click the previous button
-    // Assert offsetChange is emitted with value 0
+  it.todo('Emits offsetChange when previous is clicked', async () => {
+    const offsetChange = vi.fn<(offset: number) => void>();
+    await t.mount(RecipePaginator, {
+      inputs: { offset: 5, limit: 5, total: 7 },
+      outputs: { offsetChange },
+    });
+    const prevButton = screen.getByRole('button', { name: /previous/i });
+    await userEvent.click(prevButton);
+    expect(offsetChange).toHaveBeenCalledExactlyOnceWith(0);
   });
 });
