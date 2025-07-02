9f6279fb5b22c349078cbc57aa36dca838d17af0
Younes Jaaidi <younes@marmicode.io>
2025-07-02 12:08:27 +0200

    build: set up nx boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,12 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import nx from '@nx/eslint-plugin';
 
 export default tseslint.config(
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -26,9 +30,34 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+          depConstraints: [
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: [
+                'type:feature',
+                'type:infra',
+                'type:core',
+              ],
+            },
+            {
+              sourceTag: 'type:infra',
+              onlyDependOnLibsWithTags: ['type:infra', 'type:core'],
+            },
+            {
+              sourceTag: 'type:core',
+              onlyDependOnLibsWithTags: ['type:core'],
+            },
+          ],
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
