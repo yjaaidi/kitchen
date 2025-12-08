3558c3793caed832240daf3b7120b6b117e7bcbd
Younes Jaaidi <younes@marmicode.io>
2025-12-08 15:16:33 +0100

    feat: enhance recipe selection with checkboxes and state management

--- apps/angular-navigation/src/app/recipe-selector.ng.ts
+++ apps/angular-navigation/src/app/recipe-selector.ng.ts
@@ -1,16 +1,29 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RecipeRepository } from './recipe-repository';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { RecipeRepository } from './recipe-repository';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-recipe-selector',
+  imports: [FormsModule],
   template: `
     <h1>Recipe Selector</h1>
     <ul>
-      @for (recipe of recipes.value(); track recipe.id) {
+      @for (recipe of recipesWithChecked(); track recipe.id) {
       <li>
-        {{ recipe.name }}
+        <input
+          type="checkbox"
+          [ngModel]="recipe.isSelected"
+          (ngModelChange)="selectRecipe(recipe.id, $event)"
+        />
+        <span>{{ recipe.name }}</span>
       </li>
       }
     </ul>
@@ -22,6 +35,22 @@ export class RecipeSelector {
   recipes = rxResource({
     stream: () => this._repository.fetchRecipes(),
   });
+  selectedRecipeIds = signal<string[]>([]);
+
+  recipesWithChecked = computed(() => {
+    return (
+      this.recipes.value()?.map((recipe) => ({
+        ...recipe,
+        isSelected: this.selectedRecipeIds().includes(recipe.id),
+      })) ?? []
+    );
+  });
+
+  selectRecipe(id: string, isSelected: boolean) {
+    this.selectedRecipeIds.update((ids) =>
+      isSelected ? [...ids, id] : ids.filter((id) => id !== id)
+    );
+  }
 }
 
 export default RecipeSelector;
