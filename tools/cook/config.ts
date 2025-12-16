1ce789a95ae75db1173430fa4e56f9b7a0b9814e
Younes Jaaidi <younes@marmicode.io>
2025-12-16 16:01:26 +0100

    feat: ✨ update exercise names to include numbering

--- tools/cook/config.ts
+++ tools/cook/config.ts
@@ -3,51 +3,51 @@ import { type Config, type Exercise } from './core.ts';
 const exercises: Exercise[] = [
   {
     id: '101-display-recipes',
-    name: 'Display Recipes',
+    name: '101. Display Recipes',
   },
   {
     id: '102-filter-recipes',
-    name: 'Filter Recipes',
+    name: '102. Filter Recipes',
   },
   {
     id: '201-recipe-preview',
-    name: 'Recipe Preview',
+    name: '201. Recipe Preview',
   },
   {
     id: '202-recipe-filter',
-    name: 'Recipe Filter',
+    name: '202. Recipe Filter',
   },
   {
     id: '203-recipe-filter-two-way-binding',
-    name: 'Recipe Filter Two-Way Binding',
+    name: '203. Recipe Filter Two-Way Binding',
   },
   {
     id: '301-recipe-preview-customization',
-    name: 'Recipe Preview Customization',
+    name: '301. Recipe Preview Customization',
   },
   {
     id: '302-theming',
-    name: 'Theming',
+    name: '302. Theming',
   },
   {
     id: '303-slots',
-    name: 'Slots',
+    name: '303. Slots',
   },
   {
     id: '401-fetch-recipes',
-    name: 'Fetch Recipes',
+    name: '401. Fetch Recipes',
   },
   {
     id: '402-handle-errors',
-    name: 'Handle Errors',
+    name: '402. Handle Errors',
   },
   {
     id: '403-cancel-request',
-    name: 'Cancel Request',
+    name: '403. Cancel Request',
   },
   {
     id: '404-task',
-    name: 'Task',
+    name: '404. Task',
   },
 ];
 
