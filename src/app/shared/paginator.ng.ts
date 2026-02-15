41e46013d797f138f086d497c87971a74d268c2d
Younes Jaaidi <younes@marmicode.io>
2026-02-16 00:57:58 +0100

    feat: improve navigation buttons UI

--- src/app/shared/paginator.ng.ts
+++ src/app/shared/paginator.ng.ts
@@ -11,12 +11,63 @@ import {
   selector: 'wm-paginator',
   template: `
     @if (showPagination()) {
-      <button type="button" [disabled]="!hasPrevious()" (click)="goPrevious()">
-        Previous
-      </button>
-      <button type="button" [disabled]="!hasNext()" (click)="goNext()">
-        Next
-      </button>
+      <div class="paginator">
+        <button
+          type="button"
+          class="nav-btn"
+          [disabled]="!hasPrevious()"
+          (click)="goPrevious()"
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          class="nav-btn"
+          [disabled]="!hasNext()"
+          (click)="goNext()"
+        >
+          Next
+        </button>
+      </div>
+    }
+  `,
+  styles: `
+    :host {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+    }
+
+    .paginator {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid rgb(163, 212, 255);
+      border-radius: 9999px;
+      overflow: hidden;
+    }
+
+    .nav-btn {
+      padding: 0.5em 1em;
+      border: none;
+      border-right: 1px solid rgb(163, 212, 255);
+      background: white;
+      color: rgb(105, 105, 105);
+      cursor: pointer;
+      font: inherit;
+    }
+
+    .nav-btn:last-child {
+      border-right: none;
+    }
+
+    .nav-btn:hover:not(:disabled) {
+      background: rgb(245, 250, 255);
+    }
+
+    .nav-btn:disabled {
+      opacity: 0.5;
+      cursor: not-allowed;
     }
   `,
 })
