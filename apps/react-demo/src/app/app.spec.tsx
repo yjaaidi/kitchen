b8ba26c938a4ccfac34a1959cd764c542d5a5d91
Younes Jaaidi <younes@marmicode.io>
2026-02-21 15:28:10 +0100

    test: simplify tests

--- apps/react-demo/src/app/app.spec.tsx
+++ apps/react-demo/src/app/app.spec.tsx
@@ -1,26 +1,17 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-    );
+    const { baseElement } = render(<App />);
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getAllByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-    );
+    const { getAllByText } = render(<App />);
     expect(
-      getAllByText(new RegExp('Welcome @react-demo/react-demo', 'gi')).length >
+      getAllByText(new RegExp('Welcome @react-demo/react-demo', 'i')).length >
         0,
     ).toBeTruthy();
   });
