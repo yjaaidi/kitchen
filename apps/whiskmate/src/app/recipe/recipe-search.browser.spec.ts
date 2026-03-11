c333bd465d796870388eddd775784ab08204f9cc
Younes Jaaidi <younes@marmicode.io>
2026-03-11 11:57:22 +0100

    test: ✅ use page api

--- apps/whiskmate/src/app/recipe/recipe-search.browser.spec.ts
+++ apps/whiskmate/src/app/recipe/recipe-search.browser.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { screen } from '@testing-library/angular';
-import { userEvent } from 'vitest/browser';
 import { describe, expect, it } from 'vitest';
+import { page } from 'vitest/browser';
 import { MealPlanner } from '../meal-planner/meal-planner';
 import {
   provideRecipeRepositoryFake,
@@ -12,32 +11,25 @@ import { recipeMother } from './recipe.mother';
 
 describe(RecipeSearch, () => {
   it('show recipes', async () => {
-    const { findRecipeHeadings } = mountRecipeSearch();
+    const { recipeHeadings } = mountRecipeSearch();
 
-    const headings = await findRecipeHeadings();
-
-    expect(headings).toHaveLength(2);
-    expect(headings[0]).toHaveTextContent('Burger');
-    expect(headings[1]).toHaveTextContent('Salad');
+    await expect.element(recipeHeadings).toHaveLength(2);
+    await expect.element(recipeHeadings.nth(0)).toHaveTextContent('Burger');
+    await expect.element(recipeHeadings.nth(1)).toHaveTextContent('Salad');
   });
 
   it('filter recipes', async () => {
-    const { findRecipeHeadings, typeKeywords } = mountRecipeSearch();
+    const { recipeHeadings, typeKeywords } = mountRecipeSearch();
 
     await typeKeywords('Bur');
 
-    const headings = await findRecipeHeadings();
-
-    expect(headings).toHaveLength(1);
-    expect(headings[0]).toHaveTextContent('Burger');
+    await expect.element(recipeHeadings).toHaveTextContent('Burger');
   });
 
   it('adds recipes to meal plan', async () => {
-    const { findAddButtons, getMealPlannerRecipes } = mountRecipeSearch();
+    const { addButtons, getMealPlannerRecipes } = mountRecipeSearch();
 
-    const addButtons = await findAddButtons();
-
-    await userEvent.click(addButtons[0]);
+    await addButtons.first().click();
 
     expect(getMealPlannerRecipes()).toMatchObject([{ name: 'Burger' }]);
   });
@@ -58,11 +50,10 @@ function mountRecipeSearch() {
   TestBed.createComponent(RecipeSearch);
 
   return {
-    findRecipeHeadings: () => screen.findAllByRole('heading', { level: 2 }),
-    findAddButtons: () => screen.findAllByRole('button', { name: 'ADD' }),
+    recipeHeadings: page.getByRole('heading', { level: 2 }),
+    addButtons: page.getByRole('button', { name: 'ADD' }),
     getMealPlannerRecipes: () => TestBed.inject(MealPlanner).recipes(),
-    typeKeywords: async (keywords: string) => {
-      await userEvent.type(await screen.findByLabelText('Keywords'), keywords);
-    },
+    typeKeywords: (keywords: string) =>
+      page.getByRole('textbox', { name: 'Keywords' }).fill(keywords),
   };
 }
