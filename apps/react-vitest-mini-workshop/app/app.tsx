a4a3d38f5cbbda6a9d6e61eb4d6a6a998250b4b1
Younes Jaaidi <younes@marmicode.io>
2026-05-12 17:36:05 +0200

    Initial commit

---
+++ apps/react-vitest-mini-workshop/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@react-vitest-mini-workshop/react-vitest-mini-workshop" />
+    </div>
+  );
+}
+
+export default App;
