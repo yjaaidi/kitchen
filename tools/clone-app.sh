3c4a7e6d9b3b5283a72c2face1d5e5284de3322e
Younes Jaaidi <younes@marmicode.io>
2026-05-15 15:11:09 +0200

    build: 🛠️ fix clone-app.sh

--- tools/clone-app.sh
+++ tools/clone-app.sh
@@ -14,4 +14,6 @@ DESTINATION="${2#apps/}"
 
 cp -r apps/$SOURCE apps/$DESTINATION
 cd apps/$DESTINATION
-sed -i '' "s/$SOURCE/$DESTINATION/g" project.json
+
+PROJECT_OR_PACKAGE_FILE_NAME=$([ -f "project.json" ] && echo "project.json" || echo "package.json")
+sed -i '' "s/$SOURCE/$DESTINATION/g" $PROJECT_OR_PACKAGE_FILE_NAME
