be9e36adf5c4d1d7f50b724a1236fc3335f7a5b5
Younes Jaaidi <younes@marmicode.io>
2026-05-18 21:03:15 +0200

    docs: 📝 add some docs to singleton utils

--- apps/301-browser-mode-solution/app/util/singleton.ts
+++ apps/301-browser-mode-solution/app/util/singleton.ts
@@ -1,6 +1,13 @@
 const OVERRIDE_METHOD = Symbol('singleton:override');
 const RESET_METHOD = Symbol('singleton:reset');
 
+/**
+ * Defines a singleton using a factory function.
+ *
+ * @example
+ * const singleton = defineSingleton(() => new MyClass());
+ * const instance = singleton.get();
+ */
 export function defineSingleton<T>(factory: () => T): Singleton<T> {
   let currentFactory = factory;
   const originalFactory = factory;
@@ -38,11 +45,26 @@ interface _SingletonInternal<T> {
   [RESET_METHOD](): void;
 }
 
+/**
+ * A separate set of utilities for testing singletons.
+ * This should not be used in production code.
+ */
 export const singletonTestingUtils = {
+  /**
+   * Override a singleton with a new factory function.
+   *
+   * Note that a singleton can't be overridden if it has already been initialized.
+   *
+   * @param singleton - The singleton to override.
+   * @param factory - The factory function to override the singleton with.
+   */
   override<T>(singleton: Singleton<T>, factory: () => T): void {
     (singleton as _SingletonInternal<T>)[OVERRIDE_METHOD](factory);
   },
 
+  /**
+   * Reset all overridden singletons to their original factory functions.
+   */
   reset(): void {
     for (const s of overriddenSingletons) {
       s[RESET_METHOD]();
