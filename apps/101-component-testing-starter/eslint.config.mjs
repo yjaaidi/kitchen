a5356aa079087586ef6d562958e465b7627ea81f
Younes Jaaidi <younes@marmicode.io>
2026-05-20 09:48:38 +0200

    test: ✅ set up vitest lint rules

--- apps/101-component-testing-starter/eslint.config.mjs
+++ apps/101-component-testing-starter/eslint.config.mjs
@@ -1,12 +1,16 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
+import vitest from '@vitest/eslint-plugin';
 
 export default [
   ...nx.configs['flat/react'],
   ...baseConfig,
+  vitest.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
-    rules: {},
+    rules: {
+      'vitest/valid-title': 'off',
+    },
   },
 ];
