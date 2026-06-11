a9a6620ca50671791d700491cba2f44101162fb0
Younes Jaaidi <younes@marmicode.io>
2026-06-11 13:41:40 +0200

    test: ✅ tidy up wrappers

--- apps/whiskmate/src/app/recipe/recipe-search.browser.spec.tsx
+++ apps/whiskmate/src/app/recipe/recipe-search.browser.spec.tsx
@@ -1,15 +1,14 @@
 import { afterEach, describe, expect, it } from 'vitest';
 import { act, render, screen, waitFor, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Provider } from 'react-redux';
 import { reset } from '../shared/shared.actions';
 import { store } from '../store';
-import { createTestQueryClientWrapper } from '../testing/test-query-client';
 import { singletonTestingUtils } from '../util/singleton';
 import { recipeRepositorySingleton } from './recipe-repository';
 import { RecipeRepositoryFake } from './recipe-repository.fake';
 import { RecipeSearch } from './recipe-search';
 import { recipeMother } from './recipe.mother';
+import { createCommonTestWrapper } from '../testing/test-wrapper';
 
 describe(RecipeSearch, () => {
   afterEach(() => {
@@ -59,14 +58,8 @@ async function setUp() {
     return fake;
   });
 
-  const QueryClientWrapper = createTestQueryClientWrapper();
-
   render(<RecipeSearch />, {
-    wrapper: ({ children }) => (
-      <Provider store={store}>
-        <QueryClientWrapper>{children}</QueryClientWrapper>
-      </Provider>
-    ),
+    wrapper: createCommonTestWrapper(),
   });
 
   return {
